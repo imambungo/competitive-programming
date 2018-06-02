5b8c196e7d4348e9443c09d27033be0a58a6a319
imambungo <imambungo@gmail.com>
2018-06-03 01:14:20 +0700

    output to text file (check dummy)

--- dummy.java
+++ dummy.java
@@ -1,15 +1,26 @@
 import java.util.Scanner;
-import java.util.Arrays;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.io.File;
+
 class dummy {
-    static String sortStringByChar(String lastArmyStanding) {
-        char[] tempArray = lastArmyStanding.toCharArray();
-        Arrays.sort(tempArray);
-        String sorted = new String(tempArray);
-        return sorted;
-    }
+
     public static void main(String[] args) {
-        String a = "dcba";
-        a = sortStringByChar(a);
-        System.out.println(a);
+        try {
+            File file = new File("out.txt"); // Your file
+            FileOutputStream fos = new FileOutputStream(file);
+            PrintStream ps = new PrintStream(fos);
+            System.setOut(ps);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        
+        System.out.println("This goes to out.txt");
+        deep();
+    }
+
+    static void deep() {
+        System.out.println("sout bae");
     }
 }
