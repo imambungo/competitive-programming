2d44d4caafe856ef15f10546a531c841508b1547
imambungo <imambungo@gmail.com>
2018-06-02 22:56:59 +0700

    checkpoint

--- Kingdom Map/kingdommap.java
+++ Kingdom Map/kingdommap.java
@@ -6,12 +6,36 @@ class kingdommap{
         String wordBaru = word.substring(0, k) + '#' + word.substring(k+1, word.length());
         return wordBaru;
     }
+    static boolean checkContested(String armiesInRegion){
+        if(armiesInRegion.length() == 0){
+            return false;
+        }
+        char patokan = armiesInRegion.charAt(0);
+        for (int i = 1; i < armiesInRegion.length(); i++) {
+            if(armiesInRegion.charAt(i) != patokan){
+                return true;
+            }
+        }
+        return false;
+    }
     static void checkAt(words[] words,int i, int j, int k){
         if(words[i].word[j].charAt(k) != '#'){
             if(words[i].word[j].charAt(k) != '.'){
                 armiesInRegion += words[i].word[j].charAt(k);
             }
             words[i].word[j] = ubahCharKe(words[i].word[j], k);
+            if(j > 0){
+                checkAt(words, i, j-1, k);
+            }
+            if(k > 0){
+                checkAt(words, i, j, k-1);
+            }
+            if(j < words[i].word.length-1){
+                checkAt(words, i, j+1, k);
+            }
+            if(k < words[i].word[j].length()-1){
+                checkAt(words, i, j, k+1);
+            }
         }
         // System.out.println(words[i].word[j].charAt(k));
     }
@@ -30,12 +54,18 @@ class kingdommap{
             }
         }
         for (i = 0; i < T; i++) {
+            int contested = 0;
+            System.out.println("Case "+(i+1)+":");
             for (j = 0; j < words[i].word.length; j++) {
                 for (k = 0; k < words[i].word[j].length(); k++) {
                     checkAt(words,i,j,k);
+                    if(cekContested(armiesInRegion)){
+                        contested++;
+                    }
                     armiesInRegion = "";
                 }
             }
+            System.out.println("Contested "+contested);
         }
     }
 }
