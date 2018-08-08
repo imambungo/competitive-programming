8fcbf9d53b392e41427f959cb8d0b3bfaeaf22bf
M. Imam Pratama <imambungo@gmail.com>
2018-08-09 00:34:06 +0700

    Pake BigInteger

--- XOR A B/xorab.java
+++ XOR A B/xorab.java
@@ -1,19 +1,28 @@
 import java.util.Scanner;
+import java.math.*;
 class xorab {
-    static long a;
-    static long b;
-    static long c;
-    static long i;
+    static boolean lessEqualBigInteger(BigInteger i, BigInteger b){
+      int res = i.compareTo(b);
+      if (res==0) {
+          return true;
+      } else if(res==1) {
+          return false;
+      } else if ( res == -1){
+          return true;
+      }
+      return false;
+    }
+
     public static void main(String[] args) {
+        BigInteger a,b,c,i,d;
         Scanner sc = new Scanner(System.in);
-        String s = sc.next();
-        a = Long.parseUnsignedLong(s);
-        s = sc.next();
-        b = Long.parseUnsignedLong(s);
+        a = new BigInteger(sc.next());
+        b = new BigInteger(sc.next());
         c = a;
-        for (i = a+1; i <= b; i++) {
-            c = c^i;
+        d = a.add(BigInteger.valueOf(1));
+        for (i = d; lessEqualBigInteger(i,b); i=i.add(BigInteger.valueOf(1))) {
+            c = c.xor(i);
         }
-        System.out.println(Long.toBinaryString(c));
+        System.out.println(c.toString(2));
     }
 }
