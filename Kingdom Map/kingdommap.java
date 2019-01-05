07b038a7358af0ebb47f55eeb260e0552c360281
M. Imam Pratama <imambungo@gmail.com>
2019-01-05 15:10:58 +0700

    Make it human friendly
    Halfway there

--- Kingdom Map/kingdommap.java
+++ Kingdom Map/kingdommap.java
@@ -7,7 +7,70 @@ import java.io.PrintStream;
 import java.io.File;
 
 class kingdommap{
+	public static void main(String[] args) {
+        // to avoid stack overflow:
+        // javac kingdommap.java
+        // java -Xss4m kingdommap
+		// for linux user, to copy content of input.in, run:    xclip -sel clip < input.in
+
+		// Untuk ngeprint output ke file output.txt
+		/////////////////////////////////////////////////////////
+        try {                                                  //
+            File file = new File("output.txt");                //
+            FileOutputStream fos = new FileOutputStream(file); //
+            PrintStream ps = new PrintStream(fos);             //
+            System.setOut(ps);                                 //
+        } catch (FileNotFoundException e) {                    //
+            e.printStackTrace();                               //
+        }                                                      //
+
+        Scanner sc = new Scanner(System.in);
+        int banyakTestCase = sc.nextInt();
+        int i, j, k;
+
+
+        Map[] arrayOfMap = new Map[banyakTestCase];// Bikin array of Map
+        for (i = 0; i < banyakTestCase; i++) {
+            int N = sc.nextInt();
+            int M = sc.nextInt();
+            arrayOfMap[i] = new Map();
+            arrayOfMap[i].baris = new String[N];
+            for (j = 0; j < N; j++)
+			{
+				// Nerima String baris ke-j dari map testCase ke-j
+                arrayOfMap[i].baris[j] = sc.next();
+            }
+        }
+        for (i = 0; i < banyakTestCase; i++) {
+            int contested = 0;// Jumlah region di map i yg punya 2 atau lebih huruf yg berbeda
+            String lastArmyStanding = "";
+            System.out.println("Case "+(i+1)+":");
+
+			// Looping sebanyak baris Map testCase ke-i
+            for (j = 0; j < arrayOfMap[i].baris.length; j++)
+			{
+				// Looping sebanyak jumlah karakter dari baris ke-j Map ke-i
+                for (k = 0; k < arrayOfMap[i].baris[j].length(); k++)
+				{
+                    kirimTimPencari(arrayOfMap, i, j, k);
+                    if(checkContested()){
+                        contested++;
+                    }else if(armiesInRegion.length() != 0){
+                        lastArmyStanding += armiesInRegion.charAt(0);
+                    }
+                    armiesInRegion = "";
+                }
+            }
+            lastArmyStanding = sortStringByChar(lastArmyStanding);
+            if(lastArmyStanding.length() != 0){
+                printArmies(lastArmyStanding);
+            }
+            System.out.println("contested "+contested);
+        }
+    }
+
     static String armiesInRegion = "";
+
     static void printArmies(String lastArmyStanding){
         char patokan = lastArmyStanding.charAt(0);
         int count = 0;
@@ -24,16 +87,22 @@ class kingdommap{
             }
         }
     }
+
     static String sortStringByChar(String lastArmyStanding){
         char[] tempArray = lastArmyStanding.toCharArray();
         Arrays.sort(tempArray);
         String sorted = new String(tempArray);
         return sorted;
     }
-    static String ubahCharKe(String word, int k){
-        String wordBaru = word.substring(0, k) + '#' + word.substring(k+1, word.length());
-        return wordBaru;
+
+	// Ini adalah fungsi untuk merubah karakter ke-k dari sebuah String menjadi '#'
+    static String ubahCharKe(String baris, int k){
+        String barisBaru = baris.substring(0, k) + '#' + baris.substring(k+1);
+		// Read about substring:	https://www.javatpoint.com/substring
+        return barisBaru;
     }
+
+	// Cek apakah region di map punya 2 atau lebih huruf yg berbeda
     static boolean checkContested(){
         if(armiesInRegion.length() == 0){
             return false;
@@ -46,78 +115,58 @@ class kingdommap{
         }
         return false;
     }
-    static void checkAt(words[] words,int i, int j, int k){
-        if(words[i].word[j].charAt(k) != '#'){
-            if(words[i].word[j].charAt(k) != '.'){
-                armiesInRegion += words[i].word[j].charAt(k);
+
+	// Adalah method untuk mengecek karakter pada suatu koordinat (j,k) di map ke-i.
+		// Tidak hanya itu, setelah mengecek suatu koordinat, tim pencari akan berpencar
+		// ke segala penjuru dan menandai daerah yg sudah dicek oleh mereka
+		// sehingga daerah yg sudah dicek tidak perlu dicek ulang oleh tim pencari lain
+		// Keterangan: j -> baris; k -> kolom.
+    static void kirimTimPencari(Map[] arrayOfMap,int i, int j, int k)
+	{
+		// Kalau '#', itu berarti gunung atau jalan yg sudah ditempuh/dicek sebelumnya,
+			// jidak perlu disusuri lagi.
+        if(arrayOfMap[i].baris[j].charAt(k) != '#')
+		{
+			// Tapi kalau bukan,
+
+            if(arrayOfMap[i].baris[j].charAt(k) != '.'){
+				// Kalau bukan '.', pasti salah satu faction
+				// Masukkan karakter faction tersebut ke dalam static String daftar tim pencari
+                armiesInRegion += arrayOfMap[i].baris[j].charAt(k);
             }
-            words[i].word[j] = ubahCharKe(words[i].word[j], k);
+
+			// Tandai koordinat ini sudah dicek dengan merubah karakter ke-k pada String baris ke-j
+            arrayOfMap[i].baris[j] = ubahCharKe(arrayOfMap[i].baris[j], k);
+
+			// Lanjutkan perjalanan, berpencar.
+
+			// Selagi belum di perbatasan sebelah utara,
             if(j > 0){
-                checkAt(words, i, j-1, k);
+				// Kirim tim pencari ke arah utara
+                kirimTimPencari(arrayOfMap, i, j-1, k);
             }
+
+			// Selagi belum di perbatasan sebelah barat,
             if(k > 0){
-                checkAt(words, i, j, k-1);
+				// Kirim tim pencari ke arah barat
+                kirimTimPencari(arrayOfMap, i, j, k-1);
             }
-            if(j < words[i].word.length-1){
-                checkAt(words, i, j+1, k);
-            }
-            if(k < words[i].word[j].length()-1){
-                checkAt(words, i, j, k+1);
-            }
-        }
-        // System.out.println(words[i].word[j].charAt(k));
-    }
-    public static void main(String[] args) {
-        // to avoid stack exchange:
-        // javac kingdommap.java
-        // java -Xss4m kingdommap
 
-        try {
-            File file = new File("output.txt"); // Your file
-            FileOutputStream fos = new FileOutputStream(file);
-            PrintStream ps = new PrintStream(fos);
-            System.setOut(ps);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
+			// Selagi belum di perbatasan sebelah selatan,
+            if(j < arrayOfMap[i].baris.length-1){
+				// Kirim tim pencari ke arah selatan
+                kirimTimPencari(arrayOfMap, i, j+1, k);
+            }
 
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
-        int i,j,k;
-        words[] words = new words[T];
-        for (i = 0; i < T; i++) {
-            int N = sc.nextInt();
-            int M = sc.nextInt();
-            words[i] = new words();
-            words[i].word = new String[N];
-            for (j = 0; j < N; j++) {
-                words[i].word[j] = sc.next();
+			// Selagi belum di perbatasan sebelah timur,
+            if(k < arrayOfMap[i].baris[j].length()-1){
+				// Kirim tim pencari ke arah timur
+                kirimTimPencari(arrayOfMap, i, j, k+1);
             }
-        }
-        for (i = 0; i < T; i++) {
-            int contested = 0;
-            String lastArmyStanding = "";
-            System.out.println("Case "+(i+1)+":");
-            for (j = 0; j < words[i].word.length; j++) {
-                for (k = 0; k < words[i].word[j].length(); k++) {
-                    checkAt(words,i,j,k);
-                    if(checkContested()){
-                        contested++;
-                    }else if(armiesInRegion.length() != 0){
-                        lastArmyStanding += armiesInRegion.charAt(0);
-                    }
-                    armiesInRegion = "";
-                }
-            }
-            lastArmyStanding = sortStringByChar(lastArmyStanding);
-            if(lastArmyStanding.length() != 0){
-                printArmies(lastArmyStanding);
-            }
-            System.out.println("contested "+contested);
         }
     }
 }
 
-class words {
-    String[] word;
+class Map {
+    String[] baris;// Setiap Map punya array of baris, mulai dari baris[0] (baris pertama) dst.
 }
